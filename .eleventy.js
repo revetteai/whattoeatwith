d6bb136e82a39fcbaf78d817b91969f091397478
revetteai <revetteai@gmail.com>
2025-04-23 11:28:51 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
   // Define "post" collection from markdown files in /posts
   eleventyConfig.addCollection("post", function (collectionApi) {
     return collectionApi.getFilteredByGlob("./posts/*.md");
+  });
+
+  // Add dynamic date filter for Nunjucks
+  eleventyConfig.addFilter("date", (value, format = "yyyy") => {
+    return DateTime.fromJSDate(new Date(value)).toFormat(format);
   });
 
   return {
